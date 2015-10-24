9826f42411e3f7f4a993c13525246c2a5ed497a1
david <daviderler1995@gmail.com>
2015-10-24 18:27:48 +0200

    und noch was neues

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("das ist ein test");
+        System.out.println("Jetzt kann ich was veraendern");
     }
 }
